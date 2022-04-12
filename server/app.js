fa89e4db4fca876ca6c338e79ad858aa55e81cd2
fajardwiutomo <fajardwi576@gmail.com>
2022-04-12 18:19:14 +0700

    register and lgin feature

---
+++ server/app.js
@@ -0,0 +1,20 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors')
+const Controller = require('./controllers/controllers')
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.post('/register', Controller.register)
+app.post('/login', Controller.login)
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ server/controllers/controllers.js
@@ -0,0 +1,74 @@
+const { User } = require('../models')
+
+class Controller {
+    static async register (req, res, next){
+        try {
+            const { username, email, password } = req.body;
+            const result = await User.create({
+              username,
+              email,
+              password,
+              role: "Admin",
+            });
+            res.status(201).json({ id: result.id, email: result.email });
+          } catch (error) {
+            next(error);
+          }
+    }
+
+    static async login (req, res, next) {
+      try {
+        const { email, password }  = req.body
+
+        if (!email, !password) {
+          throw {
+            code: 400,
+            name: "Unauthorized",
+            message: "Email or Password must filled"
+          }
+        }
+
+        const emailFound = await User.findOne({
+          where: {
+            email,
+          }
+        })
+
+        if(!emailFound) {
+          throw {
+            code: 401,
+            name: "Unauthorized",
+            message: "Invalid username, email or password",
+          };
+        } else {
+          const compare = comparePass(password, emailFound.password);
+
+          if (!compare) {
+            throw {
+              code: 401,
+              name: "Unauthorized",
+              message: "Invalid username, email or password",
+            };
+          } else {
+            const payload = {
+              id: emailFound.id,
+            };
+  
+            //buat token
+            const token = tokenPayload(payload);
+            // next();
+  
+            res.status(200).json({
+              access_token: token,
+            });
+          }
+        }
+        
+      } catch (error) {
+        next(error)
+      }
+    }
+
+}
+
+module.exports = Controller
